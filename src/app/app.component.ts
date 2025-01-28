d24ff2cf466e41891590b023a7df245d23099837
snc-jbavalekar <jbavalekar@brightviewar.com>
2025-01-28 13:38:54 +0530

    changes added for login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,18 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  showHeaderFooter = true;
 
   title = 'skillmuni-version2';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Hide header on the login route
+        this.showHeaderFooter = event.url !== '/login';
+      }
+    });
   }
 }
